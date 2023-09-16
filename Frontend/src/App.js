31e0971412ede6a699207a02fbbe47703ec3af4d
gmshimon <simon.rosedale99@gmail.com>
2023-09-17 01:59:11 +0800

    Design the navbar

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -2,11 +2,14 @@ import logo from './logo.svg'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Dashboard from './Page/Dashboard/Dashboard'
+import Drawer from './Component/Drawer/Drawer'
 
 function App () {
   return (
     <div>
-      <Dashboard />
+      <Drawer>
+        <Dashboard />
+      </Drawer>
     </div>
   )
 }
---
+++ Frontend/src/Component/Drawer/Drawer.js
@@ -0,0 +1,84 @@
+import {
+  FaTh,
+  FaBars,
+  FaUserAlt,
+  FaRegChartBar,
+  FaCommentAlt,
+  FaShoppingBag,
+  FaThList
+} from 'react-icons/fa'
+import React, { useState } from 'react'
+import { NavLink } from 'react-router-dom'
+import './Drawer.css'
+const Drawer = ({ children }) => {
+  const [isOpen, setIsOpen] = useState(false)
+  const toggle = () => setIsOpen(!isOpen)
+  const menuItem = [
+    {
+      path: '/',
+      name: 'Dashboard',
+      icon: <FaTh />
+    },
+    {
+      path: '/about',
+      name: 'About',
+      icon: <FaUserAlt />
+    },
+    {
+      path: '/analytics',
+      name: 'Analytics',
+      icon: <FaRegChartBar />
+    },
+    {
+      path: '/comment',
+      name: 'Comment',
+      icon: <FaCommentAlt />
+    },
+    {
+      path: '/product',
+      name: 'Product',
+      icon: <FaShoppingBag />
+    },
+    {
+      path: '/productList',
+      name: 'Product List',
+      icon: <FaThList />
+    }
+  ]
+  return (
+    <div className='container1'>
+      <div style={{ width: isOpen ? '200px' : '50px' }} className='sidebar1'>
+        <div className='top_section1'>
+          <h1 style={{ display: isOpen ? 'block' : 'none' }} className='logo1'>
+            logo1
+          </h1>
+          <div
+            style={{ marginLeft: isOpen ? '50px' : '0px' }}
+            className='bars1'
+          >
+            <FaBars onClick={toggle} />
+          </div>
+        </div>
+        {menuItem.map((item, index) => (
+          <NavLink
+            to={item.path}
+            key={index}
+            className='link1'
+            activeclassName='active1'
+          >
+            <div className='icon1'>{item.icon}</div>
+            <div
+              style={{ display: isOpen ? 'block' : 'none' }}
+              className='link_text'
+            >
+              {item.name}
+            </div>
+          </NavLink>
+        ))}
+      </div>
+      <main>{children}</main>
+    </div>
+  )
+}
+
+export default Drawer
