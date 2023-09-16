41ecd608d628ec673f10b0650ceeaddcdf59e3dc
CatAteMaBurrito <63182939+CatAteMaBurrito@users.noreply.github.com>
2023-09-16 12:42:35 +0600

    feat: add counter cards on the dashboard homepage
    Added a counter card component on the dashboard homepage that indicate
    the total number of customers, mechanics, enquiries and feedback. This
    component merely exists as a design template in the long run.

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import DashboardCounter from './js/DashboardCounter';
+import DashboardTable from './js/DashboardTable';
+import Container from 'react-bootstrap/esm/Container';
 
 function Greeting() {
-  return <h2>My First Component</h2>;
+  return (
+    <Container>
+      <DashboardCounter />
+    </Container>
+  );
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
