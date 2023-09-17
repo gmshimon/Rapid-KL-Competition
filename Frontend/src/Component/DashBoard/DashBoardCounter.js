64ec7cb27c6c567372905a218656e31a5020e89e
CatAteMaBurrito <63182939+CatAteMaBurrito@users.noreply.github.com>
2023-09-17 22:21:30 +0600

    Change styles for dashboard cards

--- Frontend/src/Component/DashBoard/DashBoardCounter.js
+++ Frontend/src/Component/DashBoard/DashBoardCounter.js
@@ -1,59 +1,61 @@
-import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
-import Card from 'react-bootstrap/Card'
-import './css/dashboard-cards.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faUsers } from '@fortawesome/free-solid-svg-icons'
-import { faHatCowboy } from '@fortawesome/free-solid-svg-icons'
-import { faSpinner } from '@fortawesome/free-solid-svg-icons'
-import { faComments } from '@fortawesome/free-solid-svg-icons'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card';
+import './css/dashboard-cards.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUsers } from '@fortawesome/free-solid-svg-icons';
+import { faHatCowboy } from '@fortawesome/free-solid-svg-icons';
+import { faSpinner } from '@fortawesome/free-solid-svg-icons';
+import { faComments } from '@fortawesome/free-solid-svg-icons';
 
-function DashboardCounter () {
-  const totalCustomer = 3
-  const totalMechanic = 1
-  const totalEnquiry = 5
-  const totalFeedback = 1
+function DashboardCounter() {
+  const totalCustomer = 3;
+  const totalMechanic = 1;
+  const totalEnquiry = 5;
+  const totalFeedback = 1;
   return (
-    <Row>
-      <Col className='col-3 col-m-6 col-sm-6'>
-        <Card className='counter bg-customer'>
-          <Card.Body>
-            <FontAwesomeIcon icon={faUsers} />
-            <Card.Title>{totalCustomer}</Card.Title>
-            <Card.Text>Total Customer</Card.Text>
-          </Card.Body>
-        </Card>
-      </Col>
-      <Col className='col-3 col-m-6 col-sm-6'>
-        <Card className='counter bg-mechanic'>
-          <Card.Body>
-            <FontAwesomeIcon icon={faHatCowboy} />
-            <Card.Title>{totalMechanic}</Card.Title>
-            <Card.Text>Total Mechanic</Card.Text>
-          </Card.Body>
-        </Card>
-      </Col>
-      <Col className='col-3 col-m-6 col-sm-6'>
-        <Card className='counter bg-enquiry'>
-          <Card.Body>
-            <FontAwesomeIcon icon={faSpinner} />
-            <Card.Title>{totalEnquiry}</Card.Title>
-            <Card.Text>Total Enquiry</Card.Text>
-          </Card.Body>
-        </Card>
-      </Col>
-      <Col className='col-3 col-m-6 col-sm-6'>
-        <Card className='counter bg-feedback'>
-          <Card.Body>
-            <FontAwesomeIcon icon={faComments} />
-            <Card.Title>{totalFeedback}</Card.Title>
-            <Card.Text>Total Feedback</Card.Text>
-          </Card.Body>
-        </Card>
-      </Col>
-    </Row>
-  )
+    <div className="counter-cards">
+      <Row>
+        <Col className="col-3 col-m-6 col-sm-6">
+          <Card className="counter bg-customer">
+            <Card.Body>
+              <FontAwesomeIcon icon={faUsers} />
+              <Card.Title>{totalCustomer}</Card.Title>
+              <Card.Text>Total Customer</Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col className="col-3 col-m-6 col-sm-6">
+          <Card className="counter bg-mechanic">
+            <Card.Body>
+              <FontAwesomeIcon icon={faHatCowboy} />
+              <Card.Title>{totalMechanic}</Card.Title>
+              <Card.Text>Total Mechanic</Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col className="col-3 col-m-6 col-sm-6">
+          <Card className="counter bg-enquiry">
+            <Card.Body>
+              <FontAwesomeIcon icon={faSpinner} />
+              <Card.Title>{totalEnquiry}</Card.Title>
+              <Card.Text>Total Enquiry</Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col className="col-3 col-m-6 col-sm-6">
+          <Card className="counter bg-feedback">
+            <Card.Body>
+              <FontAwesomeIcon icon={faComments} />
+              <Card.Title>{totalFeedback}</Card.Title>
+              <Card.Text>Total Feedback</Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </div>
+  );
 }
 
-export default DashboardCounter
+export default DashboardCounter;
