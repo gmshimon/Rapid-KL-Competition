0de7623d8d818a358bac794b4f5b10a3bb58ebc2
gmshimon <simon.rosedale99@gmail.com>
2023-09-19 01:55:16 +0800

    Redisgn the table

--- Frontend/src/Page/Dashboard/Dashboard.js
+++ Frontend/src/Page/Dashboard/Dashboard.js
@@ -1,7 +1,9 @@
-import React from 'react';
-import Container from 'react-bootstrap/esm/Container';
-import DashboardTable from '../../Component/DashBoard/DashBoardTable';
-import DashboardCounter from '../../Component/DashBoard/DashBoardCounter';
+import React from 'react'
+import Container from 'react-bootstrap/esm/Container'
+import DashboardTable from '../../Component/DashBoard/DashBoardTable'
+import DashboardCounter from '../../Component/DashBoard/DashBoardCounter'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
 
 const Dashboard = () => {
   return (
@@ -9,7 +11,7 @@ const Dashboard = () => {
       <DashboardCounter />
       <DashboardTable />
     </Container>
-  );
-};
+  )
+}
 
-export default Dashboard;
+export default Dashboard
