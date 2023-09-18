18091122ca6c43af63c77e62a75bb7466791243f
CatAteMaBurrito <63182939+CatAteMaBurrito@users.noreply.github.com>
2023-09-18 15:49:58 +0600

    Implement analytic page modal
    Added an modal on the dashboard for the analytic page that dynamically
    changes based on the bus id clicked on the enquiry table. This should be
    better for user accessability as they are no longer routed to a new page
    upon clicking on the ids. Instead, a modal with static backdrop shows up
    containing the AnalyticPage component.

--- Frontend/src/Component/Analytics/AnalyticHeader/AnalyticHeader.js
+++ Frontend/src/Component/Analytics/AnalyticHeader/AnalyticHeader.js
@@ -1,47 +1,46 @@
-import React from 'react'
-import Card from 'react-bootstrap/Card'
+import React from 'react';
+import Card from 'react-bootstrap/Card';
 
 const AnalyticHeader = ({ data }) => {
   return (
     <section>
-      <h6 style={{ textAlign: 'center', fontSize: '25px' }}>Dashboard</h6>
-      <div className='row'>
-        <div class='col-3'>
-          <div class='card bg-customer text-white'>
-            <div class='card-body'>
-              <h5 class='card-title'>Bus id</h5>
-              <p class='card-text'>{data.bus_id}</p>
+      <div className="row">
+        <div className="col-3">
+          <div className="card bg-customer text-white">
+            <div className="card-body">
+              <h5 className="card-title">Bus id</h5>
+              <p className="card-text">{data.bus_id}</p>
             </div>
           </div>
         </div>
 
-        <div class='col-3'>
-          <div class='card bg-mechanic text-white'>
-            <div class='card-body'>
-              <h5 class='card-title'>Depot id</h5>
-              <p class='card-text'>{data.depot_id}</p>
+        <div className="col-3">
+          <div className="card bg-mechanic text-white">
+            <div className="card-body">
+              <h5 className="card-title">Depot id</h5>
+              <p className="card-text">{data.depot_id}</p>
             </div>
           </div>
         </div>
-        <div class='col-3'>
-          <div class='card bg-enquiry text-white'>
-            <div class='card-body'>
-              <h5 class='card-title'>Depot NM</h5>
-              <p class='card-text'>{data.depot_nm}</p>
+        <div className="col-3">
+          <div className="card bg-enquiry text-white">
+            <div className="card-body">
+              <h5 className="card-title">Depot NM</h5>
+              <p className="card-text">{data.depot_nm}</p>
             </div>
           </div>
         </div>
-        <div class='col-3'>
-          <div class='card bg-feedback text-white'>
-            <div class='card-body'>
-              <h5 class='card-title'>Model Num</h5>
-              <p class='card-text'>{data.model_num}</p>
+        <div className="col-3">
+          <div className="card bg-feedback text-white">
+            <div className="card-body">
+              <h5 className="card-title">Model Num</h5>
+              <p className="card-text">{data.model_num}</p>
             </div>
           </div>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default AnalyticHeader
+export default AnalyticHeader;
