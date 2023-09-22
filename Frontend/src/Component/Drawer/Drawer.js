4cd0ed9943626abbee698de50733328b65e06d82
CatAteMaBurrito <63182939+CatAteMaBurrito@users.noreply.github.com>
2023-09-23 02:13:24 +0600

    test: add mobile navbar

--- Frontend/src/Component/Drawer/Drawer.js
+++ Frontend/src/Component/Drawer/Drawer.js
@@ -1,30 +1,71 @@
-import {
-  FaTh,
-  FaBars,
-  FaUserAlt,
-  FaRegChartBar,
-  FaCommentAlt,
-  FaShoppingBag,
-  FaTools
-} from 'react-icons/fa'
-import { MdFeedback } from 'react-icons/md'
-import React, { useState } from 'react'
-import { NavLink } from 'react-router-dom'
-import './Drawer.css'
+import { FaTh, FaBars, FaTools } from 'react-icons/fa';
+import { MdFeedback } from 'react-icons/md';
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import './Drawer.css';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+
+function OffCanvasNavbar() {
+  return (
+    <>
+      {[false].map((expand) => (
+        <Navbar key={expand} expand={expand} className="mb-3">
+          <Container fluid>
+            <Navbar.Brand href="#" style={{ fontSize: '21px' }}>
+              MaintainIQ
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
+            <Navbar.Offcanvas
+              id={`offcanvasNavbar-expand-${expand}`}
+              aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+              placement="start"
+            >
+              <Offcanvas.Header closeButton>
+                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
+                  MaintainIQ
+                </Offcanvas.Title>
+              </Offcanvas.Header>
+              <Offcanvas.Body>
+                <Nav className="justify-content-end flex-grow-1 pe-3">
+                  <Nav.Link href="/">
+                    <FaTh className="navbar-elements" />
+                    Dashboard
+                  </Nav.Link>
+                  <Nav.Link href="/parts">
+                    <FaTools className="navbar-elements" />
+                    Inventory
+                  </Nav.Link>
+                  <Nav.Link href="/feedback">
+                    <MdFeedback className="navbar-elements" />
+                    Feedback
+                  </Nav.Link>
+                </Nav>
+              </Offcanvas.Body>
+            </Navbar.Offcanvas>
+          </Container>
+        </Navbar>
+      ))}
+    </>
+  );
+}
+
 const Drawer = ({ children }) => {
   // console.log(children)
-  const [isOpen, setIsOpen] = useState(true)
-  const toggle = () => setIsOpen(!isOpen)
+  const [isOpen, setIsOpen] = useState(true);
+  const toggle = () => setIsOpen(!isOpen);
   const menuItem = [
     {
       path: '/',
       name: 'Dashboard',
-      icon: <FaTh />
+      icon: <FaTh />,
     },
     {
       path: '/parts',
       name: 'Inventory',
-      icon: <FaTools />
+      icon: <FaTools />,
     },
     // {
     //   path: '/analytics',
@@ -45,43 +86,49 @@ const Drawer = ({ children }) => {
     {
       path: '/feedback',
       name: 'Feedback',
-      icon: <MdFeedback />
-    }
-  ]
+      icon: <MdFeedback />,
+    },
+  ];
   return (
-    <div className='container1'>
-      <div style={{ width: isOpen ? '200px' : '50px' }} className='sidebar1'>
-        <div className='top_section1'>
-          <h1 style={{ display: isOpen ? 'block' : 'none' }} className='logo1'>
-            MaintainIQ
-          </h1>
-          <div
-            style={{ marginLeft: isOpen ? '40px' : '0px' }}
-            className='bars1'
-          >
-            <FaBars onClick={toggle} />
-          </div>
-        </div>
-        {menuItem.map((item, index) => (
-          <NavLink
-            to={item.path}
-            key={index}
-            className='link1'
-            activeclassName='active1'
-          >
-            <div className='icon1'>{item.icon}</div>
-            <div
+    <>
+      {OffCanvasNavbar()}
+      <div className="container1">
+        <div style={{ width: isOpen ? '200px' : '50px' }} className="sidebar1">
+          <div className="top_section1">
+            <h1
               style={{ display: isOpen ? 'block' : 'none' }}
-              className='link_text'
+              className="logo1"
             >
-              {item.name}
+              MaintainIQ
+            </h1>
+            <div
+              style={{ marginLeft: isOpen ? '40px' : '0px' }}
+              className="bars1"
+            >
+              <FaBars onClick={toggle} />
             </div>
-          </NavLink>
-        ))}
+          </div>
+          {menuItem.map((item, index) => (
+            <NavLink
+              to={item.path}
+              key={index}
+              className="link1"
+              activeclassName="active1"
+            >
+              <div className="icon1">{item.icon}</div>
+              <div
+                style={{ display: isOpen ? 'block' : 'none' }}
+                className="link_text"
+              >
+                {item.name}
+              </div>
+            </NavLink>
+          ))}
+        </div>
+        <main>{children}</main>
       </div>
-      <main>{children}</main>
-    </div>
-  )
-}
+    </>
+  );
+};
 
-export default Drawer
+export default Drawer;
